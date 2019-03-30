42f2a37ac5875fc02accdc7259770977b11635e5
Satyam Kumar <io.satyamtg@gmail.com>
2019-03-30 12:13:07 +0530

    Add getfakeratio()

--- updownvote.py
+++ updownvote.py
@@ -8,11 +8,31 @@ authentication = FirebaseAuthentication(SECRET,EMAIL, True, True)
 firebase = FirebaseApplication(DSN, authentication)
 
 def upvote(idno):
-    upvote = firebase.get("/articles/"+idno+"/upvote", None)
+    upvote = firebase.get("/articles/"+idno+"/upvote/", None)
+    if(upvote == None):
+        firebase.patch("/articles/"+idno, {'upvote': '1'})
+    #print("/articles/"+idno+"/upvote")
+    #print(upvote)
     upvote = upvote+1
     firebase.patch("/articles/"+idno, {'upvote': upvote})
 
 def downvote(idno):
-    downvote = firebase.get("/articles/"+idno+"/downvote", None)
-    downvote = downvote-1
-    firebase.patch("/articles/"+idno, {'downvote': downvote})    
+    downvote = firebase.get("/articles/"+idno+"/downvote/", None)
+    if(downvote == None):
+        firebase.patch("/articles/"+idno, {'downvote': '1'})
+    downvote = downvote+1
+    firebase.patch("/articles/"+idno, {'downvote': downvote})
+
+def getfakeratio(idno):
+    upvote = firebase.get("/articles/"+idno+"/upvote/", None)   
+    downvote = firebase.get("/articles/"+idno+"/downvote/", None)
+    fakeratio = downvote/(upvote+downvote)
+    print(fakeratio)
+    return fakeratio
+
+#Test
+#upvote("1")
+#upvote("2")
+#downvote("3")
+#getfakeratio("1")
+
