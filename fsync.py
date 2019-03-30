39f1560592edbd26a5f3e0bf1ecb119ec5683ee3
Satyam Kumar <io.satyamtg@gmail.com>
2019-03-30 23:36:11 +0530

    More materialization

--- fsync.py
+++ fsync.py
@@ -7,14 +7,9 @@ EMAIL = 'abhishek.tiwari3507@gmail.com'
 authentication = FirebaseAuthentication(SECRET,EMAIL, True, True)
 firebase = FirebaseApplication(DSN, authentication)
 
-<<<<<<< HEAD
-def uploadarticle(idno, body, headline, author):
-    data={'body' :body, 'author' :author, 'headline':headline}
-=======
 def uploadarticle(idno, body, headline, author, downvote, upvote,score):
     #changing score to str beacuse json is not float32 serialisable
     data={'body' :body, 'author' :author, 'headline':headline,'ml_score':str(score), 'idno': idno,'downvote':downvote, 'upvote':upvote}
->>>>>>> aea8c646d7920f4704bee8a3aa9893b9c62a2939
     putpath="/articles/"+str(idno)
     firebase.patch(putpath, data)
 
